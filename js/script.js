dd665144175f792ad5ac814e05690410894b9e29
KamilObrzut <127477561+KamilObrzut@users.noreply.github.com>
2023-08-07 12:38:50 +0200

    Fixed responsive
    Fixed responsive

--- js/script.js
+++ js/script.js
@@ -1,16 +1,6 @@
-
-//const SCENE_ELEMENT_ID = "scene";
-
-//const sceneElement = document.getElementById(SCENE_ELEMENT_ID);
-//const parallaxInstance = new Parallax(sceneElement);
-
-
 const translate = document.querySelectorAll(".translate");
 const translateRight = document.querySelectorAll(".translateRight");
 const translateLeft = document.querySelectorAll(".translateLeft");
-const translateBorderOne = document.querySelectorAll(".translateBorderOne");
-const translateBorderTwo = document.querySelectorAll(".translateBorderTwo");
-const translateBorderThree = document.querySelectorAll(".translateBorderThree");
 
 window.addEventListener('scroll', () => {
     let scroll = window.pageYOffset;
@@ -27,17 +17,5 @@ window.addEventListener('scroll', () => {
         let speed = element.dataset.speed;
         speed = -speed
         element.style.transform = `translateX(${scroll * speed}px)`;
-    })
-    translateBorderOne.forEach(element => {
-        let speed = element.dataset.speed;
-        element.style.width = `${scroll * speed + 50}px`;
-    })
-    translateBorderTwo.forEach(element => {
-        let speed = element.dataset.speed;
-        element.style.width = `${scroll * speed + 60}px`;
-    })
-    translateBorderThree.forEach(element => {
-        let speed = element.dataset.speed;
-        element.style.width = `${scroll * speed}px`;
     })
 })
