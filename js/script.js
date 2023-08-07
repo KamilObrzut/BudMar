c6591a7d3193cfeb04cd65056a2e5e851d670043
KamilObrzut <127477561+KamilObrzut@users.noreply.github.com>
2023-08-07 15:50:50 +0200

    Added Carousel for Gallery
    Added Carousel for Gallery

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
+//Header
+
 const translate = document.querySelectorAll(".translate");
 const translateRight = document.querySelectorAll(".translateRight");
 const translateLeft = document.querySelectorAll(".translateLeft");
 const menuIcon = document.querySelector(".menu-icon")
 const expandMenu = document.querySelector(".expand-menu")
+const carousel = document.querySelector('.carousel');
 
 window.addEventListener('scroll', () => {
     let scroll = window.pageYOffset;
@@ -27,3 +30,33 @@ const funcExpandMenu = () => {
 }
 
 menuIcon.addEventListener('click', funcExpandMenu)
+
+//Carousel
+
+const slides = document.querySelectorAll('.carousel-one-slide');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+    slides[currentSlide].style.opacity = 0;
+    slides[currentSlide].style.display = 'none';
+
+    currentSlide = (slideIndex + slides.length) % slides.length;
+
+    slides[currentSlide].style.display = 'block';
+    slides[currentSlide].style.opacity = 1;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+nextButton.addEventListener('click', nextSlide);
+prevButton.addEventListener('click', prevSlide);
+
+showSlide(currentSlide);
